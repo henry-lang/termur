d56be526a57aafeb4ea71c1a33fd8d4ce303ab15
Henry Langmack <Henry.langmack@gmail.com>
2022-01-07 21:32:05 -0500

    beginning of the tui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::{io, thread, time};
+use termion::raw::IntoRawMode;
+use tui::Terminal;
+use tui::backend::TermionBackend;
+use tui::widgets::{Widget, Block, Borders};
+use tui::layout::{Layout, Constraint, Direction};
+
+fn main() -> Result<(), io::Error> {
+    let stdout = io::stdout().into_raw_mode()?;
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.clear()?;
+    
+    terminal.draw(|f| {
+        let size = f.size();
+        let block = Block::default()
+            .title("Termur")
+            .borders(Borders::ALL);
+        f.render_widget(block, size);
+    })?;
+    
+    thread::sleep(time::Duration::from_millis(2000));
+
+    Ok(())
 }
